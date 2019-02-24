e7f96ce503de985248eaf5e4e9e70d8be93d8c2b
rozsabence <robence@gmail.com>
2019-02-24 16:31:18 +0100

    add select song action handler to button

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,8 @@
-export const selectSong = (song) => {
+const selectSong = (song) => {
   return {
     type: 'SONG_SELECTED',
     payload: song,
   };
 };
+
+export default selectSong;
--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import selectSong from '../actions';
+
 const SongList = (props) => {
   const renderList = () => {
     return props.songs.map((song) => {
       return (
         <div className="item" key={song.title}>
           <div className="right floated content">
-            <button className="ui button primary">Select</button>
+            <button
+              className="ui button primary"
+              onClick={() => props.selectSong(song)}
+            >
+              Select
+            </button>
           </div>
           <div className="content">{song.title}</div>
         </div>
@@ -21,4 +28,7 @@ const mapStateToProps = (state) => {
   return { songs: state.songs };
 };
 
-export default connect(mapStateToProps)(SongList);
+export default connect(
+  mapStateToProps,
+  { selectSong },
+)(SongList);
